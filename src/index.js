a9e5352aa2db19a7f056a24e350e5a54d407e84c
Aitzhan <aitzhan@mail.ru>
2021-07-23 23:30:27 +0600

    init: initial config

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import css from './main.scss';
+
+const App = () => <p>Todo App</p>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
