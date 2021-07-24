6fa67f4c3133c0bedf801d732e06c3cdf0168e01
Aitzhan <aitzhan@mail.ru>
2021-07-24 17:58:46 +0600

    fix: add styles imports to index (for uui work)

--- src/components/todo-header/TodoHeader.jsx
+++ src/components/todo-header/TodoHeader.jsx
@@ -1,29 +1,22 @@
 import React from 'react';
-// import { Text } from "@epam/loveship";
-// import { FlexRow } from "@epam/uui-components";
-
-// export default function TodoHeader() {
-//   return (
-//     <div>
-//       <FlexRow
-//         size="24"
-//       >
-//         <Text
-//           fontSize="16"
-//           lineHeight="24"
-//           size="24"
-//         >
-//           Header
-//         </Text>
-//       </FlexRow>
-//     </div>
-//   );
-// }
+import { Text } from '@epam/loveship';
+import { FlexRow } from '@epam/uui-components';
 
 export default function TodoHeader() {
   return (
     <div>
-      Header
+      <FlexRow
+        size="24"
+      >
+        <Text
+          font="sans-semibold"
+          fontSize="16"
+          lineHeight="24"
+          size="48"
+        >
+          Text Header
+        </Text>
+      </FlexRow>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './main.scss';
+
+import '@epam/uui-components/styles.css';
+import '@epam/loveship/styles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
