d3ca24a41284ad932bade63e89a24f4a0dcb804f
Aitzhan <aitzhan@mail.ru>
2021-08-22 15:33:07 +0600

    fix: change case in file names

--- src/components/tabs/Tabs.jsx
+++ src/components/tabs/Tabs.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useMemo } from 'react';
+import React, { useContext, useMemo } from 'react';
 import { TabButton, FlexRow, FlexCell } from '@epam/loveship';
 
 import { FILTERS, ACTIONS_TYPES } from '../consts/consts';
